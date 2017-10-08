faebfea039c9bbc2dd2f178246b85ca729e4b815
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2017-10-08 01:45:33 -0500

    removed mothers lastname from products add form

--- src/app/product/modules/productStampsInk.add/productStampsInk.add.ctrl.js
+++ src/app/product/modules/productStampsInk.add/productStampsInk.add.ctrl.js
@@ -49,7 +49,7 @@ module.exports = (function (angular) {
                     if (angular.isObject(promise.data)) {
                         var client = promise.data[0].cl_jsonb;
                         var cl_type = client.cl_type
-                        $scope.client = (cl_type === 'legal') ? client.cl_corporatename : client.cl_name + ' ' + client.cl_fatherslastname + ' ' + client.cl_motherslastname;                    }
+                        $scope.client = (cl_type === 'legal') ? client.cl_corporatename : client.cl_name + ' ' + client.cl_fatherslastname;                    }
                 });
 
                 productStampsInkAddFac.getMaterials().then(function (promise) {
