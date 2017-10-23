fc2aec070d1a8e91472791c103d23b52297cf5f7
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2017-10-22 23:48:58 -0500

    added status 14 to tlr query

--- sql/tlr/tlr.sql
+++ sql/tlr/tlr.sql
@@ -38,7 +38,7 @@ right join lateral (
         wo_email text, 
         wo_status int
 	)
-where (wo_jsonb->>'wo_status')::int in (0,1,2,3,4,5,6,7,8,9,10,11,12,13)
+where (wo_jsonb->>'wo_status')::int in (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14)
 ) wo
 on wo.cl_id = cl.cl_id
 order by wo.wo_date desc;
