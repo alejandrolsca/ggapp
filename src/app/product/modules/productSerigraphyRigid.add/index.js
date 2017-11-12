a75ceb0241f1967db7f26cd282bcad9291a8fe46
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2017-11-12 10:56:44 -0600

    added serigraphy rigid product

---
+++ src/app/product/modules/productSerigraphyRigid.add/index.js
@@ -0,0 +1,22 @@
+module.exports = (function(angular){
+    'use strict';
+    
+    return angular.module('app.productSerigraphyRigid.add',[])
+
+    .config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('productSerigraphyRigidAdd', {
+            url:'/product/add/serigraphy/rigid/:cl_id',
+            template: require('./productSerigraphyRigid.add.view.html'),
+            controller : 'productSerigraphyRigidAddCtrl',
+            data: {
+                requiresLogin: true
+            }    
+        });
+    }])
+
+    .factory('productSerigraphyRigidAddFac',require('./productSerigraphyRigid.add.fac'))
+
+    .controller('productSerigraphyRigidAddCtrl',require('./productSerigraphyRigid.add.ctrl'))
+
+})(angular);
