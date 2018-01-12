a13b761e5c52e9b63c5c66610a8e893786a78a5a
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2018-01-12 01:50:42 -0600

    updated product detail feature in wo add, update, duplicate and view

--- src/app/wo/modules/wo.view/wo.view.ctrl.js
+++ src/app/wo/modules/wo.view/wo.view.ctrl.js
@@ -13,106 +13,6 @@ module.exports = (function (angular) {
                 });
                 return camelCase.join('')
             }
-
-            // product modal
-            $('#modal').on('show.bs.modal', async (event) => {
-                //var button = $(event.relatedTarget); // Button that triggered the modal
-                //$scope.qrcodeString = button.data('code_data');// Extract info from data-* attributes
-                try {
-                    const { data } = await woViewFactory.getProductInfo($scope.fmData.pr_id)
-                    let hasComponents = false
-                    let componentsIndex = undefined
-                    let pr_process = undefined
-                    let pr_type = undefined
-                    data.map((value, index, data) => {
-                        if (value.key === 'pr_components') {
-                            hasComponents = true
-                            componentsIndex = index
-                        }
-                        if (value.key === 'pr_process') {
-                            pr_process = value.value
-                        }
-                        if (value.key === 'pr_type') {
-                            pr_type = value.value
-                        }
-                    })
-                    if (hasComponents) {
-                        const componentsData = data.map(async (value, index, data) => {
-                            if (typeof value.value === 'object') {
-                                value.value = null
-                            }
-                            if (value.key === 'mt_id') {
-                                for (let i = 1; i <= data[componentsIndex].value; i++) {
-                                    const { data } = await woViewFactory.getProductInfoMaterial(value['component' + i])
-                                    value['component' + i] = data[0].material
-                                }
-                            }
-                            if (value.key === 'pr_inksfront') {
-                                for (let i = 1; i <= data[componentsIndex].value; i++) {
-                                    if (value['component' + i]) {
-                                        const { data } = await woViewFactory.getProductInfoInks(Object.values(value['component' + i]).join(','))
-                                        value['component' + i] = data[0].inks
-                                    }
-                                }
-                            }
-                            if (value.key === 'pr_inksback') {
-                                for (let i = 1; i <= data[componentsIndex].value; i++) {
-                                    if (value['component' + i]) {
-                                        const { data } = await woViewFactory.getProductInfoInks(Object.values(value['component' + i]).join(','))
-                                        value['component' + i] = data[0].inks
-                                    }
-                                }
-                            }
-                            return value
-                        })
-                        componentsData.map(async (value, index, data) => {
-                            if (typeof value.value === 'object') {
-                                value.value = null
-                            }
-                            return value
-                        })
-                        Promise.all(componentsData).then((completed) => {
-                            completed.map((value) => {
-                                value.key = i18nFilter(`${camelCase('product', pr_process, pr_type)}-add.labels.${value.key.replace('_', '-')}`);
-                            })
-                            $scope.prInfo = completed
-                            $scope.components = new Array(completed[componentsIndex].value)
-                            $scope.$apply()
-                        })
-                    }
-                    if (!hasComponents) {
-                        const generalData = data.map(async (value, index, data) => {
-                            if (value.key === 'mt_id') {
-                                const { data } = await woViewFactory.getProductInfoMaterial(value.value)
-                                value.value = data[0].material
-                            }
-                            if (value.key === 'pr_inksfront') {
-                                if (value.value) {
-                                    const { data } = await woViewFactory.getProductInfoInks(Object.values(value.value).join(','))
-                                    value.value = data[0].inks
-                                }
-                            }
-                            if (value.key === 'pr_inksback') {
-                                if (value.value) {
-                                    const { data } = await woViewFactory.getProductInfoInks(Object.values(value.value).join(','))
-                                    value.value = data[0].inks
-                                }
-                            }
-                            return value
-                        })
-                        Promise.all(generalData).then((completed) => {
-                            completed.map((value) => {
-                                value.key = i18nFilter(`${camelCase('product', pr_process, pr_type)}-add.labels.${value.key.replace('_', '-')}`);
-                            })
-                            $scope.prInfo = completed
-                            $scope.components = new Array()
-                            $scope.$apply()
-                        })
-                    }
-                } catch (error) {
-                    console.log(error)
-                }
-            })
             
             $scope.wo_foliosperformatoptions = i18nFilter("wo-add.fields.wo_foliosperformatoptions");
             $scope.wo_currencyoptions = i18nFilter("wo-add.fields.wo_currencyoptions");
@@ -152,6 +52,7 @@ module.exports = (function (angular) {
                                     } else {
                                         $scope.prinfo = true;
                                         $scope.product = product[0];
+                                        $scope.productUpdateUrl = `#/product/update/${$scope.product['pr_jsonb']['pr_process']}/${$scope.product['pr_jsonb']['pr_type']}/${$scope.product['pr_jsonb']['cl_id']}/${$scope.product['pr_id']}`
                                         $scope.folio = (product[0]['pr_jsonb']['pr_folio'] === 'yes') ? true : false;
                                         var pr_type = product[0]['pr_jsonb']['pr_type'] 
                                         $scope.components = (pr_type === 'paginated' || pr_type === 'counterfoil') ? true : false;
