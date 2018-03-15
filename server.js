a4d4cdf5267bae3727666b067ec4e83fd179aa41
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2018-03-14 23:00:30 -0600

    removed invoiced api

--- server.js
+++ server.js
@@ -1467,31 +1467,6 @@ if (cluster.isMaster) {
 
     /* SHIPPING LIST */
 
-    app.post('/api/einvoice/invoiced', function (req, res, next) {
-        (async () => {
-            // note: we don't try/catch this because if connecting throws an exception
-            // we don't need to dispose of the client (it will be undefined)
-            const client = await pool.connect()
-            try {
-                // set default time zone
-                const timezone = req.body.timezone || defaultTimezone
-                await client.query(`set timezone = '${timezone}';`)
-                // execute query
-                const query = file('einvoice/wo:cl_id:wo_id')
-                const parameters = [req.body.cl_id, req.body.wo_id]
-                const { rows } = await client.query(query, parameters)
-                res.send(")]}',\n".concat(JSON.stringify(rows)));
-            } catch (e) {
-                console.log(e)
-                return res.status(500).send(JSON.stringify(e, null, 4));
-            } finally {
-                client.release()
-            }
-        })().catch(e => console.error(e.stack))
-    });
-
-    /* SHIPPING LIST */
-
     app.post('/api/shippinglist/client/cl_id', function (req, res, next) {
         (async () => {
             // note: we don't try/catch this because if connecting throws an exception
