222b51d48f3461bb8ed390481d49a4ef728c2036
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2018-06-23 19:38:04 -0500

    fixed issue that allowed disabled material when duplicating a wo

--- sql/product/product:cl_id.sql
+++ sql/product/product:cl_id.sql
@@ -1,7 +1,7 @@
 select 
     *,
 	case 
-		when jsonb_typeof(pr_jsonb->'mt_id') = 'object'
+		when jsonb_typeof(pr_jsonb.mt_id) = 'object'
 		then (
 			select 
 				array_to_string(
@@ -11,13 +11,14 @@ select
 						(mt_jsonb->>'mt_height') || 
 						(mt_jsonb->>'mt_measure') || ' ' ||
 						(
-									case 
-										when mt_jsonb->>'mt_measure' = 'in'
-										then round((((mt_jsonb->>'mt_width')::decimal*2.54)*((mt_jsonb->>'mt_height')::decimal*2.54))/10000,2)
-										else round(((mt_jsonb->>'mt_width')::decimal*(mt_jsonb->>'mt_height')::decimal)/10000,2)
-									end
+							case 
+								when mt_jsonb->>'mt_measure' = 'in'
+								then round((((mt_jsonb->>'mt_width')::decimal*2.54)*((mt_jsonb->>'mt_height')::decimal*2.54))/10000,2)
+								else round(((mt_jsonb->>'mt_width')::decimal*(mt_jsonb->>'mt_height')::decimal)/10000,2)
+							end
 						)  || 'm2'  || ' ' ||
-						(mt_jsonb->>'mt_description')
+						(mt_jsonb->>'mt_description') || '|' ||
+						(mt_jsonb->>'mt_status')
 					),', '
 				) as material
 			from (
@@ -27,7 +28,7 @@ select
 					array_to_string(array_agg(mt.value),', ')
 				from (
 					select 
-						(jsonb_each(pr.pr_jsonb->'mt_id')).*
+						(jsonb_each(pr_jsonb.mt_id)).*
 				) mt),',')::int[]) with ordinality t(mt_id, ord) using (mt_id)
 				order by t.ord
 			) mt
@@ -45,14 +46,16 @@ select
 								else round(((mt_jsonb->>'mt_width')::decimal*(mt_jsonb->>'mt_height')::decimal)/10000,2)
 							end
 				) || 'm2'  || ' ' ||
-				(mt_jsonb->>'mt_description')
+				(mt_jsonb->>'mt_description') || '|' ||
+				(mt_jsonb->>'mt_status')
 			from material 
-			where mt_id = (pr.pr_jsonb->>'mt_id')::integer
+			where mt_id = (pr_jsonb->>'mt_id')::integer
 		)
 	end as pr_material
 from  public.product pr, 
-jsonb_to_record(pr_jsonb) as x (
+jsonb_to_record(pr_jsonb) as pr_jsonb (
     cl_id int,
+	mt_id jsonb,
     pr_partno text,
     pr_code text,
     pr_name text,
@@ -62,5 +65,5 @@ jsonb_to_record(pr_jsonb) as x (
     pr_folio text,
     pr_status text
 )
-where pr_jsonb->>'cl_id' = $1
-and pr_jsonb->>'pr_status' = any(string_to_array($2,',')::text[])
+where pr_jsonb.cl_id = $1
+and pr_jsonb.pr_status = any(string_to_array($2,',')::text[])
