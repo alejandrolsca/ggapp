38ab6923a501eda4483f6fa20e40c454b26d2ed7
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2018-09-22 23:52:49 -0500

    fixed dashboard query

--- sql/dashboard/dashboard:deliveredwo.sql
+++ sql/dashboard/dashboard:deliveredwo.sql
@@ -15,12 +15,22 @@ select
 				then 1 else 0 end
 			else 0 end
 	),0) as latedelivered,
+	case when COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
+			then 1 else 0 end
+		),0) = 0 
+		then (0)::decimal(5,2)
+	else
 	(((COALESCE(sum(
 		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
 			then case when (wo_deliverydate > wo_commitmentdate) 
 				then 1 else 0 end
 			else 0 end
-	),0))::numeric * 100) / count(pjb.pr_process)::numeric)::decimal(5,2) as latepercentage,
+	),0))::numeric * 100) / (COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
+		then 1 else 0 end
+	),0))::numeric)::decimal(5,2) 
+	end as latepercentage,
 	count(pjb.pr_process) as total
 from wo wo,
 jsonb_to_record(wo_jsonb) as wjb (
