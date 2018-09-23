6c8d3cac731bc6655b786a283880fd01a77cad5e
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2018-09-22 23:21:18 -0500

    added late delivery to dashboard

--- sql/dashboard/dashboard:deliveredwo.sql
+++ sql/dashboard/dashboard:deliveredwo.sql
@@ -1,14 +1,33 @@
 select
 	pjb.pr_process,
-	count(wo_deliverydate) as delivered,
-	count(pjb.pr_process) as total,
-	((count(wo_deliverydate)::numeric * 100) / count(pjb.pr_process)::numeric)::decimal(5,2) as percentage
+	COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
+		then 1 else 0 end
+	),0) as delivered,
+	((count(wo_deliverydate)::numeric * 100) / count(pjb.pr_process)::numeric)::decimal(5,2) as percentage,
+	COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = false) 
+		then 1 else 0 end
+	),0) as notdelivered,
+	COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
+			then case when (wo_deliverydate > wo_commitmentdate) 
+				then 1 else 0 end
+			else 0 end
+	),0) as latedelivered,
+	(((COALESCE(sum(
+		case when ((wo.wo_jsonb ? 'wo_deliverydate') = true) 
+			then case when (wo_deliverydate > wo_commitmentdate) 
+				then 1 else 0 end
+			else 0 end
+	),0))::numeric * 100) / count(pjb.pr_process)::numeric)::decimal(5,2) as latepercentage,
+	count(pjb.pr_process) as total
 from wo wo,
 jsonb_to_record(wo_jsonb) as wjb (
 	pr_id int,
 	wo_status text,
 	wo_commitmentdate date,
-	wo_deliverydate timestamptz
+	wo_deliverydate date
 )
 join product pr
 on wjb.pr_id = pr.pr_id,
