06d76f014cf3e048216f86d4e9e6eff34c5f08fc
Gaspar Alejandro Sanchez Betancourt <Alejandro@Gaspars-Mac-Pro.local>
2019-10-01 00:54:40 -0500

    Added history new_changes column

--- sql/wo/wo:history.sql
+++ sql/wo/wo:history.sql
@@ -5,7 +5,8 @@ select
 	wohi.wohi_newjsonb->'wo_updatedby' wo_updatedby,
 	to_char((wohi.wohi_date at time zone 'america/chihuahua'),'YYYY-MM-DD HH24:MI:SS') as wo_updated,
 	wohi.wohi_prevjsonb->'wo_status' old_status,
-	wohi.wohi_newjsonb->'wo_status' new_status
+	wohi.wohi_newjsonb->'wo_status' new_status,
+	jsonb_diff_val(wohi_prevjsonb, wohi_newjsonb) as new_changes
 from wohistory wohi
 left join wo wo
 on wohi.wo_id = wo.wo_id
