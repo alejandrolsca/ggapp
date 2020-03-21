c2854ae0d85dcc1324e033be82b2925526cd1bcb
Gaspar Alejandro Sanchez Betancourt <Alejandro@Gaspars-Mac-Pro.local>
2020-03-21 17:18:10 -0600

    Fixed :(node:63351) UnhandledPromiseRejectionWarning: Error: Connection terminated at Connection.con.once

--- server.js
+++ server.js
@@ -84,7 +84,17 @@ if (cluster.isMaster) {
 
     //SETUP POSTGRESQL
     const pool = new Pool({
-        connectionString: process.env.PG_CONNECTION_STRING
+        connectionString: process.env.PG_CONNECTION_STRING,
+        // number of milliseconds to wait before timing out when connecting a new client
+        // by default this is 0 which means no timeout
+        connectionTimeoutMillis: 0,
+        // number of milliseconds a client must sit idle in the pool and not be checked out
+        // before it is disconnected from the backend and discarded
+        // default is 10000 (10 seconds) - set to 0 to disable auto-disconnection of idle clients
+        idleTimeoutMillis: 0,
+        // maximum number of clients the pool should contain
+        // by default this is set to 10.
+        max: 10
     })
 
     function pgToString(value) {
